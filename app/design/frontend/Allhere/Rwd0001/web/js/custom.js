2db9ffc0575462bd3890da889297e1039ca1be1d
sumit <sumit.kumar.namdeo@gmail.com>
2018-08-19 17:37:53 +0530

    adding theme files

---
+++ app/design/frontend/Allhere/Rwd0001/web/js/custom.js
@@ -0,0 +1,14 @@
+    require([
+        'jquery'
+    ], function ($) {
+    	  $(".dropdown").hover(            
+		        function() {
+		            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+		            $(this).toggleClass('open');        
+		        },
+		        function() {
+		            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+		            $(this).toggleClass('open');       
+		        }
+		    );
+    });
